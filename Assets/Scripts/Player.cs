a191e51aff494349263d99708b3f6e7c4f5e8b33
qulick <58820014+Qulick-k@users.noreply.github.com>
2023-03-24 01:37:01 +0800

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : NetworkBehaviour, IKitchenObjectParent {
 
     // [SerializeField]
     //[se]
+    public int sss ;
     public static event EventHandler OnAnyPlayerSpawned;
     public static event EventHandler OnAnyPickedSomething;
 
